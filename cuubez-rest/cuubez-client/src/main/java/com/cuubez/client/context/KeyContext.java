134c4070f3824490902383c2115c098b9c4948c8
randikaudara@gmail.com <randikaudara@gmail.com@e12a6ff3-6c03-e055-6f88-9e55e6ff6be0>
2014-02-03 17:31:22 +0000

    working cuubez-key module merged

---
+++ cuubez-rest/cuubez-client/src/main/java/com/cuubez/client/context/KeyContext.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package com.cuubez.client.context;
+
+import java.util.Arrays;
+
+import com.cuubez.key.exchange.IDHOriginator;
+import com.cuubez.key.exchange.ecmqv.ECMQVOriginator;
+
+/**
+ * @author ruwan
+ *
+ */
+public class KeyContext {
+
+	private byte[] clientPublicKey;
+	private byte[] clientPublicKey2;
+	private byte[] serverPublicKey;
+	private byte[] serverPublicKey2;
+	
+	private byte[] sharedSecretKey;
+	private ECMQVOriginator ecmvqOriginator; 
+
+	/**
+	 * @return the clientPublicKey
+	 */
+	public byte[] getClientPublicKey() {
+		return clientPublicKey;
+	}
+
+	/**
+	 * @param clientPublicKey the clientPublicKey to set
+	 */
+	public void setClientPublicKey(byte[] clientPublicKey) {
+		this.clientPublicKey = clientPublicKey;
+	}
+
+	/**
+	 * @return the serverPublicKey
+	 */
+	public byte[] getServerPublicKey() {
+		return serverPublicKey;
+	}
+
+	/**
+	 * @param serverPublicKey the serverPublicKey to set
+	 */
+	public void setServerPublicKey(byte[] serverPublicKey) {
+		this.serverPublicKey = serverPublicKey;
+	}
+
+	/**
+	 * @return the sharedSecretKey
+	 */
+	public byte[] getSharedSecretKey() {
+		return sharedSecretKey;
+	}
+
+	/**
+	 * @param sharedSecretKey the sharedSecretKey to set
+	 */
+	public void setSharedSecretKey(byte[] sharedSecretKey) {
+		this.sharedSecretKey = sharedSecretKey;
+	}
+
+	/**
+	 * @return the clientPublicKey2
+	 */
+	public byte[] getClientPublicKey2() {
+		return clientPublicKey2;
+	}
+
+	/**
+	 * @param clientPublicKey2 the clientPublicKey2 to set
+	 */
+	public void setClientPublicKey2(byte[] clientPublicKey2) {
+		this.clientPublicKey2 = clientPublicKey2;
+	}
+
+	/**
+	 * @return the ecmvqOriginator
+	 */
+	public ECMQVOriginator getEcmvqOriginator() {
+		return ecmvqOriginator;
+	}
+
+	/**
+	 * @param ecmvqOriginator the ecmvqOriginator to set
+	 */
+	public void setEcmvqOriginator(ECMQVOriginator ecmvqOriginator) {
+		this.ecmvqOriginator = ecmvqOriginator;
+	}
+
+	/**
+	 * @return the serverPublicKey2
+	 */
+	public byte[] getServerPublicKey2() {
+		return serverPublicKey2;
+	}
+
+	/**
+	 * @param serverPublicKey2 the serverPublicKey2 to set
+	 */
+	public void setServerPublicKey2(byte[] serverPublicKey2) {
+		this.serverPublicKey2 = serverPublicKey2;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "KeyContext [clientPublicKey="
+				+ Arrays.toString(clientPublicKey) + ", clientPublicKey2="
+				+ Arrays.toString(clientPublicKey2) + ", serverPublicKey="
+				+ Arrays.toString(serverPublicKey) + ", serverPublicKey2="
+				+ Arrays.toString(serverPublicKey2) + ", sharedSecretKey="
+				+ Arrays.toString(sharedSecretKey) + ", ecmvqOriginator="
+				+ ecmvqOriginator + "]";
+	}
+	
+}
